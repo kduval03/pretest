f3b3f3c81906fb4ef8725dfb2f4a2a1b0e5c09a6
Kohlton Duvall <kduval03@uafs.edu>
2026-08-20 19:39:15 -0500

    update

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -3,9 +3,28 @@ package prob1;
 public class Prob1 {
 
 	public static void main(String[] args) {
-	System.out.print("Hello World!\n");
+	int[] arr = {4,7,2,9,1};
 	
+	System.out.println(findMaxDifference(arr));
 
 	}
+	
+	public static int findMaxDifference(int[] numbers) {
+		
+		int currentMax = 0;
+		if(numbers.length < 2) {
+			return 0;
+		} else {
+		for(int i = 0; i < numbers.length - 1; i++) {
+			int findMax = Math.abs(numbers[i] - numbers[i + 1]);
+			
+			if(findMax > currentMax) {
+				currentMax = findMax;
+			}
+		}
+		return currentMax;
+	}
+		
+}
 
 }
--- pretest/prob2/Prob2.java
+++ pretest/prob2/Prob2.java
@@ -3,8 +3,35 @@ package prob2;
 public class Prob2 {
 
 	public static void main(String[] args) {
-		
+		System.out.print(countVowelSequence("Steam engine in heavy rain"));
 
+	}
+	public static int countVowelSequence(String str) {
+		
+		char[] vowels = {'a','e','i','o','u'};
+		char[] str1 = new char[str.length()];
+		
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) != ' '){
+				str1[i] == str.charAt(i);
+			}
+		}
+		
+		
+		int vowelPairs = 0;
+		
+		for(int i = 0; i < str.length()-1; i ++) {
+			for(int j = 0; j < vowels.length; j++) {
+				if(str.charAt(i) == vowels[j] && str.charAt(i+1) == vowels[j]) {
+					vowelPairs += 1;
+				}
+				
+			}
+		}
+		
+		
+		return vowelPairs;
+		
 	}
 
 }
